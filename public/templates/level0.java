ad994543588161385592b98361a07169edb09bf1
jleow <jleow@connexity.com>
2017-09-01 13:28:00 -0700

    Improved level0 code template.

--- public/templates/level0.java
+++ public/templates/level0.java
@@ -1,19 +1,23 @@
-// A previous programmer had made this Roomba too clever
+// A previous programmer had made this robot too clever
 // and now, all it does is go round and round and round
 // the green circle.
 // Can you help the robot get to the green circle?
-
-package org.jointheleague.ecolban.cleverrobot;
 
 import org.jointheleague.ecolban.rpirobot.*;
 
 public class CleverRobot extends IRobotAdapter {
 	public CleverRobot(IRobotInterface iRobot) throws Exception {
 		super(iRobot);
+
+		// Move forward
 		driveDirect(500, 500);
 		Thread.sleep(1975);
+
+		// Turn left
 		driveDirect(-100, 100);
 		Thread.sleep(1820);
+
+		// Move in a circle
 		drive(500, -1000);
 	}
 
--- public/templates/level0.py
+++ public/templates/level0.py
@@ -1,4 +1,4 @@
-# A previous programmer had made this Roomba too clever
+# A previous programmer had made this robot too clever
 # and now, all it does is go round and round and round
 # the green circle.
 # Can you help the robot get to the green circle?
@@ -7,8 +7,14 @@ from time import sleep
 from org.jointheleague.ecolban.rpirobot import SimpleIRobot
 
 robot = SimpleIRobot()
+
+# Move forward
 robot.driveDirect(500, 500)
 sleep(1.975)
+
+# Turn left
 robot.driveDirect(-100, 100)
 sleep(1.820)
+
+# Move in a circle
 robot.drive(500, -1000)
