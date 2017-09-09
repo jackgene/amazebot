b8bd134a9a08a4377e419d6e1ba2f61261522624
jleow <jleow@connexity.com>
2017-09-09 07:35:17 -0700

    More Jython debugging logging.

--- app/engines/Python.scala
+++ app/engines/Python.scala
@@ -126,11 +126,10 @@ case object Python extends Language {
             throw new InterruptedException
 
           case "java.lang.ThreadDeath" =>
+            println(s"DEBUGGING: e.cause = ${e.getCause}")
             throw new ThreadDeath
 
           case _ =>
-            println(s"DEBUGGING: e.cause = ${e.getCause}")
-            println(s"DEBUGGING: e.value = ${e.value}")
             System.err.println(
               e.toString.replaceAll(s"""${instrumentFuncName}\\([0-9]+\\);""", "")
             )
